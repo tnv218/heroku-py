b9f4ebe52b43b1c05495c53d9602ab0ef18697f8
tnv218 <tnv218@gmail.com>
2021-12-28 22:47:07 +0700

    u

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = "localhost"
-serverPort = 80
+serverPort = 81
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
